1e4088f210b4ab7283df71310b9cbad58d39eb6d
Thong Nguyen <playerthong@gmail.com>
2020-08-31 01:12:48 +0700

    Remove space in code

--- app/src/main/java/com/playerthong/androidnews/viewmodel/NewsViewModel.kt
+++ app/src/main/java/com/playerthong/androidnews/viewmodel/NewsViewModel.kt
@@ -1,10 +1,6 @@
 package com.playerthong.androidnews.viewmodel
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import com.playerthong.androidnews.api.ApiResponse
-import com.playerthong.androidnews.model.NewsApiResponse
 import com.playerthong.androidnews.model.NewsModel
 import com.playerthong.androidnews.repository.NewsRepository
 import com.playerthong.androidnews.utils.ViewState
@@ -14,12 +10,6 @@ class NewsViewModel : BaseViewModel() {
     val repository:NewsRepository=NewsRepository()
 
     val listNews = MutableLiveData<List<NewsModel>>()
-
-
-
-
-
-
 
     fun getListNews(page:Int){
         viewState.postValue(ViewState.LOADING)
